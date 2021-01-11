60498b4636843c443db576427736beea73442b47
Vladimir Murashkin <savgdev@gmail.com>
2021-01-11 23:00:13 +0300

    folder structure improved

---
+++ scripts/controls.ps1
@@ -0,0 +1,87 @@
+﻿Add-Type -Path ".\src\Shpick.Wpf\bin\Debug\Shpick.Wpf.dll"
+Add-Type -Path ".\src\Shpick.Wpf\bin\Debug\Shpick.Models.dll"
+
+#TODO: to cmdlets
+function CheckBox()
+{
+    [OutputType([Shpick.Models.IParameterSpec])]
+    param(
+        [Parameter(Position = 1)]
+        [string] $Name
+    )
+    return [Shpick.Models.CheckBoxSpec]@{ Name = $name }
+}
+
+function TextBox()
+{
+    [OutputType([Shpick.Models.IParameterSpec])]
+    param(
+        [Parameter(Position = 1)]
+        [string] $Name
+    )
+    return [Shpick.Models.TextBoxSpec]@{ Name = $name }
+}
+
+function ComboBox()
+{
+    [OutputType([Shpick.Models.IParameterSpec])]
+    param(
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string] $Name,
+        [string] $DisplayMemberPath,
+        [ScriptBlock] $ItemsSource
+    )
+
+    return [Shpick.Models.ComboBoxSpec]@{
+        Name = $Name;
+        ItemsSource = $ItemsSource.Invoke();
+        DisplayMemberPath = $DisplayMemberPath}
+}
+
+function Get-Parameters
+{
+    param (
+        [Parameter(Position = 1)]
+        [Shpick.Models.IParameterSpec[]] $Parameters
+    )
+
+    $pickedParameters = [Shpick.Models.ParameterStream]::new()
+
+    $script = {
+        param(
+            $specs,
+            $stream
+        )
+        $picker = [Shpick.Wpf.ParametersPicker]::new($specs, $stream)
+    }
+
+    $ps = [PowerShell]::Create().
+            AddScript($script).
+            AddArgument($Parameters).
+            AddArgument($pickedParameters)
+
+    $runspace = [RunspaceFactory]::CreateRunspace()
+    $runspace.ApartmentState = "STA"
+    $runspace.ThreadOptions = "ReuseThread"
+    $runspace.Open()
+
+    $ps.Runspace = $runspace
+    $asyncResult = $ps.BeginInvoke()
+
+    start-sleep -seconds 1
+    if ($ps.HadErrors)
+    {
+        $ps.Streams.Error | Out-String | Write-host
+    }
+    else
+    {
+        try
+        {
+            return $pickedParameters
+        }
+        catch
+        {
+            $error | Out-String | Write-host
+        }
+    }
+}
---
+++ scripts/example.ps1
@@ -0,0 +1,13 @@
+﻿$DebugPreference = "Continue"
+$ErrorActionPreference = "Stop"
+
+. ./scripts/controls.ps1
+. ./scripts/Test-Cmdlet.ps1
+
+Get-Parameters @(
+CheckBox "Toggle"
+TextBox "Name"
+TextBox "IntValue"
+ComboBox "Date" -ItemsSource { Get-Date }
+ComboBox "Process" -DisplayMemberPath "Name" -ItemsSource { Get-Process -Name *powershell* }
+) | % { Test-Cmdlet @_ }
