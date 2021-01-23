5ac9c7f0984b02324887c3342844a189e59276a1
Vladimir Murashkin <savgdev@gmail.com>
2021-01-23 11:35:01 +0300

    Value from pipeline for "Name" parameter in controls

--- scripts/GetParameter/GetParameter.psm1
+++ scripts/GetParameter/GetParameter.psm1
@@ -6,7 +6,7 @@ function New-CheckBox()
 {
     [OutputType([Shpick.Models.IParameterSpec])]
     param(
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, ValueFromPipeline)]
         [string] $Name
     )
     return [Shpick.Models.CheckBoxSpec]@{ Name = $name }
@@ -16,7 +16,7 @@ function New-TextBox()
 {
     [OutputType([Shpick.Models.IParameterSpec])]
     param(
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, ValueFromPipeline)]
         [string] $Name
     )
     return [Shpick.Models.TextBoxSpec]@{ Name = $name }
@@ -35,53 +35,65 @@ function New-ComboBox()
     return [Shpick.Models.ComboBoxSpec]@{
         Name = $Name;
         ItemsSource = $ItemsSource.Invoke();
-        DisplayMemberPath = $DisplayMemberPath}
+        DisplayMemberPath = $DisplayMemberPath
+    }
+}
+
+function Open-WindowRunspace()
+{
+    $runspace = [RunspaceFactory]::CreateRunspace()
+    $runspace.ApartmentState = "STA"
+    $runspace.ThreadOptions = "ReuseThread"
+    $runspace.Open()
+    return $runspace
 }
 
 function Get-Parameter
 {
+    [CmdletBinding()]
     param (
         [Parameter(Position = 1)]
         [Shpick.Models.IParameterSpec[]] $Parameters
     )
 
-    $pickedParameters = [Shpick.Models.ParameterStream]::new()
+    process{
+        $pickedParameters = [Shpick.Models.ParameterStream]::new()
 
-    $script = {
-        param(
-            $specs,
-            $stream
-        )
-        $picker = [Shpick.Wpf.ParametersPicker]::new($specs, $stream)
-    }
-
-    $ps = [PowerShell]::Create().
-            AddScript($script).
-            AddArgument($Parameters).
-            AddArgument($pickedParameters)
-
-    $runspace = [RunspaceFactory]::CreateRunspace()
-    $runspace.ApartmentState = "STA"
-    $runspace.ThreadOptions = "ReuseThread"
-    $runspace.Open()
-
-    $ps.Runspace = $runspace
-    $asyncResult = $ps.BeginInvoke()
-
-    start-sleep -seconds 1
-    if ($ps.HadErrors)
-    {
-        $ps.Streams.Error | Out-String | Write-host
-    }
-    else
-    {
-        try
-        {
-            return $pickedParameters
+        $script = {
+            param(
+                $specs,
+                $stream
+            )
+            $picker = [Shpick.Wpf.ParametersPicker]::new($specs, $stream)
         }
-        catch
+
+        $ps = [PowerShell]::Create().
+                AddScript($script).
+                AddArgument($Parameters).
+                AddArgument($pickedParameters)
+
+        $runspace = Open-WindowRunspace([void])
+        $ps.Runspace = $runspace
+        $asyncResult = $ps.BeginInvoke()
+
+        start-sleep -seconds 1
+        if ($ps.HadErrors)
         {
-            $error | Out-String | Write-host
+            $ps.Streams.Error | Out-String | Write-host
+        }
+        else
+        {
+            try
+            {
+                return $pickedParameters
+                Write-Host "Window closed"
+            }
+            catch
+            {
+                $error | Out-String | Write-host
+                $ps.Dispose()
+            }
         }
     }
+    
 }
